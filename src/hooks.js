7b01e65a122f04e022fb3b50ee4e70670ad07079
BayuDC <dev.bayudc@gmail.com>
2022-02-25 10:08:39 +0700

    Redirect unauthenticated user

---
+++ src/hooks.js
@@ -0,0 +1,16 @@
+import { apiUrl } from '$lib/config';
+
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
+    if (event.url.pathname != '/auth') {
+        const res = await fetch(apiUrl + '/auth', {
+            headers: { Cookie: event.request.headers.get('Cookie') },
+        });
+
+        if (!res.ok) {
+            return Response.redirect(event.url.origin + '/auth');
+        }
+    }
+
+    return await resolve(event);
+}
