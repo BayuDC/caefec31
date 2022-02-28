d473fd50b10a55abc88ef43ec8a75ccd44a7efe2
BayuDC <dev.bayudc@gmail.com>
2022-02-28 09:47:41 +0700

    Handle api gateway timeout

--- src/hooks.js
+++ src/hooks.js
@@ -2,17 +2,24 @@ import { apiUrl } from '$lib/config';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-    const res = await fetch(apiUrl + '/auth', {
-        headers: { Cookie: event.request.headers.get('Cookie') },
-        method: 'head',
-    });
+    try {
+        const res = await fetch(apiUrl + '/auth', {
+            headers: { Cookie: event.request.headers.get('Cookie') },
+            method: 'head',
+        });
 
-    if (!res.ok && event.url.pathname != '/auth') {
-        return Response.redirect(event.url.origin + '/auth');
-    }
-    if (res.ok && event.url.pathname == '/auth') {
-        return Response.redirect(event.url.origin);
-    }
+        if (!res.ok && event.url.pathname != '/auth') {
+            return Response.redirect(event.url.origin + '/auth');
+        }
+        if (res.ok && event.url.pathname == '/auth') {
+            return Response.redirect(event.url.origin);
+        }
 
-    return await resolve(event);
+        return await resolve(event);
+    } catch (err) {
+        return new Response(`<h1 style="font-size: 40px">504: Can't connect to Waifuseum API</h1>`, {
+            headers: { 'Content-type': 'text/html' },
+            status: 504,
+        });
+    }
 }
